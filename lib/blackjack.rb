85ce417dd287cc0f4039892d02188934d23ba674
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-30 15:22:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,11 @@ def end_game(card_total)
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+
+  sum = card1 + card2
+  display_card_total(sum)
 end
 
 def hit?
